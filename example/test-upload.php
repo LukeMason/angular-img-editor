b9b1f0610b7c17adcceca9d0e7de752655331ce4
LukeMason <lukemason9@gmail.com>
2014-06-02 21:56:49 -0400

    dynamic file name

--- example/test-upload.php
+++ example/test-upload.php
@@ -2,10 +2,11 @@
     // requires php5
     define('UPLOAD_DIR', '../images/');
     $img = $_POST['base64'];
+    $name = $_POST['base64'];
     $img = str_replace('data:image/jpeg;base64,', '', $img);
     $img = str_replace(' ', '+', $img);
     $data = base64_decode($img);
-    $file = UPLOAD_DIR . 'test.jpeg';
+    $file = UPLOAD_DIR . $name . '.jpeg';
     $success = file_put_contents($file, $data);
-    print $success ? $file : ;
+    print $success ? $file : 'Could Not Create File';
 ?>
